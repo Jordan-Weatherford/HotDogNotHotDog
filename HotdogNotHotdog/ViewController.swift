2a70e08bb87a818af6ecc55f28deb572c6ffcea7
Mikayla Jordan <hippypanda@gmail.com>
2017-05-19 14:09:08 -0700

    added face detection (maybe)

--- HotdogNotHotdog/ViewController.swift
+++ HotdogNotHotdog/ViewController.swift
@@ -9,19 +9,28 @@
 
 import UIKit
 import AVFoundation
+import VisualRecognitionV3
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCapturePhotoCaptureDelegate {
     
     @IBOutlet weak var tempImageView: UIImageView!
     @IBOutlet weak var cameraView: UIView!
-    
+	
+	// Array of TagItems
+	var tagItems: [TagItem] = []
+	
     var captureSession : AVCaptureSession?
     var stillImageOutput : AVCapturePhotoOutput?
     var previewLayer : AVCaptureVideoPreviewLayer?
     var image : UIImage?
-    
+	var visualRecognition: VisualRecognition!
+	
+	let apiKey = "04789d85b44bb93565faec141ce20f83c055e77b"
+	let version = "2017-05-19"
+	
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+		
         previewLayer?.frame = cameraView.bounds
     }
     
@@ -72,10 +81,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecJPEG])
             stillImageOutput?.capturePhoto(with: photoSettings, delegate: self)
         }
+		
+		classifyImage()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		visualRecognition = VisualRecognition(apiKey: apiKey,
+		                                      version: version)
     }
 
     override func didReceiveMemoryWarning() {
@@ -96,22 +109,115 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
         
     }
-    
+	
+	func failVisualRecognitionWithError(_ error: Error) {
+		// Print the error to the console
+		print(error)
+		// Present an alert to the user describing what the problem may be
+		
+	}
+	
+	func classifyImage() {
+		print("i'm heeeree!!")
+		// String that will hold the result name from Watson
+		var resultName: String!
+		// Double that will hold the result scored from Watson
+		var resultScore: Double!
+		// String that will hold the result score percentage from Watson
+		var resultScorePercentage: String!
+		// Classify image using Visual Recognition
+		
+		let recogURL = URL(string: "https://pbs.twimg.com/profile_images/558109954561679360/j1f9DiJi.jpeg")!
+		visualRecognition.detectFaces(inImageFile: recogURL, failure: failVisualRecognitionWithError) {
+			detectedFaces in
+			// Loop through detected faces
+			for detectedFace in detectedFaces.images {
+				// Loop through the faces found in detectedFaces
+				for face in detectedFace.faces {
+					// Set the result name, score and score percentage
+					// Handle optional min and max ages
+					if(((face.age.min) != nil) && ((face.age.max) != nil)) {
+						resultName = face.gender.gender + " (" + String(describing: face.age.min!) + "-" + String(describing: face.age.max!) + ")"
+						resultScorePercentage = String(Int(round(face.gender.score * 100))) + "% (" + String(Int(round(face.age.score * 100))) + "%)"
+					}
+					else if((face.age.min) != nil) {
+						resultName = face.gender.gender + " (" + String(describing: face.age.min!) + "-?)"
+						resultScorePercentage = String(Int(round(face.gender.score * 100))) + "% (" + String(Int(round(face.age.score * 100))) + "%)"
+						
+					}
+					else if((face.age.max) != nil) {
+						resultName = face.gender.gender + " (?-" + String(describing: face.age.max!) + ")"
+						resultScorePercentage = String(Int(round(face.gender.score * 100))) + "% (" + String(Int(round(face.age.score * 100))) + "%)"
+					}
+					else {
+						resultName = face.gender.gender
+						resultScorePercentage = String(Int(round(face.gender.score * 100)))
+					}
+					resultScore = face.gender.score
+					// Create new tag item based on result name, score and score percentage
+					let newTagItem = TagItem(watsonResultName: resultName, watsonResultScore: resultScore, watsonResultScorePercentage: resultScorePercentage)
+					// Append the new tag item to the tag items array
+					self.tagItems.append(newTagItem)
+					// If celebrity match is found add set the result name, score, and score percentage
+					if(face.identity != nil){
+						resultName = face.identity?.name
+						resultScore = face.identity?.score
+						resultScorePercentage = String(Int(round(resultScore * 100))) + "% "
+						let newTagItem = TagItem(watsonResultName: resultName,
+						                         watsonResultScore: resultScore,
+						                         watsonResultScorePercentage: resultScorePercentage)
+						// Append the new tag item to the tag items array
+						self.tagItems.append(newTagItem)
+						print("hi")
+						
+					}
+				}
+			}
+			
+			DispatchQueue.main.sync
+				{
+
+				}
+			}
+		}
+	
+//	let recogURL = URL(string: "https://unsplash.it/50/100?image=\(randumNumber)")!
+//		visualRecognition.classify(image: recogURL.absoluteString, failure: failure) {
+//			classifiedImages in
+//			if let classifiedImage = classifiedImages.images.first {
+//				print(classifiedImage.classifiers)
+//
+//				if let classification = classifiedImage.classifiers.first?.classes.first?.classification {
+//					DispatchQueue.main.async {
+//						self.navigationItem.title = classification
+//						button.isEnabled = true
+//					}
+//
+//				}
+//			}else{
+//				DispatchQueue.main.async {
+//					self.navigationItem.title = "could not be determined"
+//					button.isEnabled = true
+//				}
+//			}
+//		}
+//	}
+
     @IBAction func captureButtonPressed(_ sender: UIButton) {
         didPressTakePhoto()
-        
+		
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
 }
 
